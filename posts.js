f71d713f4e27d809ceef67c5f8ee18d352635610
Andreas Nuss <nuss@yy.local>
2025-09-18 12:55:59 +0200

    easier linking

---
+++ posts.js
@@ -0,0 +1,18 @@
+const posts = [
+  {
+    href: "2025-09-18-12:39-we-get-better.html",
+    title: "only up",
+  },
+  {
+    href: "posts/2025-09-18-skill-of-patience.html",
+    title: "patience",
+  },
+  {
+    href: "posts/2025-09-18-careful-what-you-wish-for.html",
+    title: "careful what you wish for",
+  },
+  {
+    href: "posts/2025-09-17-first-post.html",
+    title: "My First Post",
+  },
+];
