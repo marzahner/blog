e6daf2e792cf1f658050eeb427bd395aaedcffdd
Andreas Nuss <nuss@yy.local>
2025-09-27 15:50:16 +0200

    added dates

--- posts.js
+++ posts.js
@@ -1,30 +1,37 @@
 const posts = [
   {
-    href: "posts/2025-09-27-13:16-the-long-bedtime.html",
+    href: "posts/the-long-bedtime.html",
     title: "requirements monster",
+    date: "2025-09-27・13:16",
   },
   {
-    href: "posts/2025-09-21-23:40-the-kanji-writer.html",
+    href: "posts/the-kanji-writer.html",
     title: "we dont learn",
+    date: "2025-09-21・23:40",
   },
   {
-    href: "posts/2025-09-21-20:23-learning-is-same.html",
+    href: "posts/learning-is-same.html",
     title: "we learn",
+    date: "2025-09-21・20:23",
   },
   {
-    href: "posts/2025-09-18-12:39-we-get-better.html",
+    href: "posts/we-get-better.html",
     title: "only up",
+    date: "2025-09-18・12:39",
   },
   {
-    href: "posts/2025-09-18-skill-of-patience.html",
+    href: "posts/skill-of-patience.html",
     title: "patience",
+    date: "2025-09-18・10:57",
   },
   {
-    href: "posts/2025-09-18-careful-what-you-wish-for.html",
+    href: "posts/careful-what-you-wish-for.html",
     title: "careful wishing",
+    date: "2025-09-18・00:35",
   },
   {
-    href: "posts/2025-09-17-first-post.html",
+    href: "posts/first-post.html",
     title: "first post",
+    date: "2025-09-17・17:11",
   },
 ];
