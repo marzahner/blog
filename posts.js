d97593bf8ba7186b85c59eb1e2cca1aa4fb42355
Andreas Nuss <nuss@yy.local>
2025-10-03 21:41:21 +0200

    backup scrollable version added

--- posts.js
+++ posts.js
@@ -49,4 +49,254 @@ const posts = [
     title: "first post",
     date: "2025-09-17・17:11",
   },
+  {
+    href: "posts/we-fat-we-stupit.html",
+    title: "we fat we stupit",
+    date: "2025-10-03・20:25",
+  },
+  {
+    href: "posts/ich-raste.html",
+    title: "ICH RASTE",
+    date: "2025-09-30・21:32",
+  },
+  {
+    href: "posts/saettigung.html",
+    title: "「sättigung」について",
+    date: "2025-09-30・12:20",
+  },
+  {
+    href: "posts/the-long-bedtime.html",
+    title: "requirements monster",
+    date: "2025-09-27・13:16",
+  },
+  {
+    href: "posts/the-kanji-writer.html",
+    title: "we dont learn",
+    date: "2025-09-21・23:40",
+  },
+  {
+    href: "posts/learning-is-same.html",
+    title: "we learn",
+    date: "2025-09-21・20:23",
+  },
+  {
+    href: "posts/we-get-better.html",
+    title: "only up",
+    date: "2025-09-18・12:39",
+  },
+  {
+    href: "posts/skill-of-patience.html",
+    title: "patience",
+    date: "2025-09-18・10:57",
+  },
+  {
+    href: "posts/careful-wishing.html",
+    title: "careful wishing",
+    date: "2025-09-18・00:35",
+  },
+  {
+    href: "posts/first-post.html",
+    title: "first post",
+    date: "2025-09-17・17:11",
+  },
+  {
+    href: "posts/we-fat-we-stupit.html",
+    title: "we fat we stupit",
+    date: "2025-10-03・20:25",
+  },
+  {
+    href: "posts/ich-raste.html",
+    title: "ICH RASTE",
+    date: "2025-09-30・21:32",
+  },
+  {
+    href: "posts/saettigung.html",
+    title: "「sättigung」について",
+    date: "2025-09-30・12:20",
+  },
+  {
+    href: "posts/the-long-bedtime.html",
+    title: "requirements monster",
+    date: "2025-09-27・13:16",
+  },
+  {
+    href: "posts/the-kanji-writer.html",
+    title: "we dont learn",
+    date: "2025-09-21・23:40",
+  },
+  {
+    href: "posts/learning-is-same.html",
+    title: "we learn",
+    date: "2025-09-21・20:23",
+  },
+  {
+    href: "posts/we-get-better.html",
+    title: "only up",
+    date: "2025-09-18・12:39",
+  },
+  {
+    href: "posts/skill-of-patience.html",
+    title: "patience",
+    date: "2025-09-18・10:57",
+  },
+  {
+    href: "posts/careful-wishing.html",
+    title: "careful wishing",
+    date: "2025-09-18・00:35",
+  },
+  {
+    href: "posts/first-post.html",
+    title: "first post",
+    date: "2025-09-17・17:11",
+  },
+  {
+    href: "posts/we-fat-we-stupit.html",
+    title: "we fat we stupit",
+    date: "2025-10-03・20:25",
+  },
+  {
+    href: "posts/ich-raste.html",
+    title: "ICH RASTE",
+    date: "2025-09-30・21:32",
+  },
+  {
+    href: "posts/saettigung.html",
+    title: "「sättigung」について",
+    date: "2025-09-30・12:20",
+  },
+  {
+    href: "posts/the-long-bedtime.html",
+    title: "requirements monster",
+    date: "2025-09-27・13:16",
+  },
+  {
+    href: "posts/the-kanji-writer.html",
+    title: "we dont learn",
+    date: "2025-09-21・23:40",
+  },
+  {
+    href: "posts/learning-is-same.html",
+    title: "we learn",
+    date: "2025-09-21・20:23",
+  },
+  {
+    href: "posts/we-get-better.html",
+    title: "only up",
+    date: "2025-09-18・12:39",
+  },
+  {
+    href: "posts/skill-of-patience.html",
+    title: "patience",
+    date: "2025-09-18・10:57",
+  },
+  {
+    href: "posts/careful-wishing.html",
+    title: "careful wishing",
+    date: "2025-09-18・00:35",
+  },
+  {
+    href: "posts/first-post.html",
+    title: "first post",
+    date: "2025-09-17・17:11",
+  },
+  {
+    href: "posts/we-fat-we-stupit.html",
+    title: "we fat we stupit",
+    date: "2025-10-03・20:25",
+  },
+  {
+    href: "posts/ich-raste.html",
+    title: "ICH RASTE",
+    date: "2025-09-30・21:32",
+  },
+  {
+    href: "posts/saettigung.html",
+    title: "「sättigung」について",
+    date: "2025-09-30・12:20",
+  },
+  {
+    href: "posts/the-long-bedtime.html",
+    title: "requirements monster",
+    date: "2025-09-27・13:16",
+  },
+  {
+    href: "posts/the-kanji-writer.html",
+    title: "we dont learn",
+    date: "2025-09-21・23:40",
+  },
+  {
+    href: "posts/learning-is-same.html",
+    title: "we learn",
+    date: "2025-09-21・20:23",
+  },
+  {
+    href: "posts/we-get-better.html",
+    title: "only up",
+    date: "2025-09-18・12:39",
+  },
+  {
+    href: "posts/skill-of-patience.html",
+    title: "patience",
+    date: "2025-09-18・10:57",
+  },
+  {
+    href: "posts/careful-wishing.html",
+    title: "careful wishing",
+    date: "2025-09-18・00:35",
+  },
+  {
+    href: "posts/first-post.html",
+    title: "first post",
+    date: "2025-09-17・17:11",
+  },
+  {
+    href: "posts/we-fat-we-stupit.html",
+    title: "we fat we stupit",
+    date: "2025-10-03・20:25",
+  },
+  {
+    href: "posts/ich-raste.html",
+    title: "ICH RASTE",
+    date: "2025-09-30・21:32",
+  },
+  {
+    href: "posts/saettigung.html",
+    title: "「sättigung」について",
+    date: "2025-09-30・12:20",
+  },
+  {
+    href: "posts/the-long-bedtime.html",
+    title: "requirements monster",
+    date: "2025-09-27・13:16",
+  },
+  {
+    href: "posts/the-kanji-writer.html",
+    title: "we dont learn",
+    date: "2025-09-21・23:40",
+  },
+  {
+    href: "posts/learning-is-same.html",
+    title: "we learn",
+    date: "2025-09-21・20:23",
+  },
+  {
+    href: "posts/we-get-better.html",
+    title: "only up",
+    date: "2025-09-18・12:39",
+  },
+  {
+    href: "posts/skill-of-patience.html",
+    title: "patience",
+    date: "2025-09-18・10:57",
+  },
+  {
+    href: "posts/careful-wishing.html",
+    title: "careful wishing",
+    date: "2025-09-18・00:35",
+  },
+  {
+    href: "posts/first-post.html",
+    title: "first post",
+    date: "2025-09-17・17:11",
+  },
 ];
